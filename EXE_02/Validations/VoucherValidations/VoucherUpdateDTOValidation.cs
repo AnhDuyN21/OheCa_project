bceb90d6fff7913a17e96878ed4c80446a188a89
congdanh1z9 <congdangdanh@gmail.com>
2024-08-03 10:51:47 +0700

    update voucher

--- EXE_02/Validations/VoucherValidations/VoucherUpdateDTOValidation.cs
+++ EXE_02/Validations/VoucherValidations/VoucherUpdateDTOValidation.cs
@@ -12,14 +12,14 @@ namespace EXE_02.Validations.VoucherValidations
                     .NotNull().WithMessage("Discount cannot be null.")
                     .InclusiveBetween(0, 100).WithMessage("Discount must be between 0 and 1.");
 
-            RuleFor(x => x.StartTime)
-                .NotNull().WithMessage("StartTime cannot be null.")
-                .GreaterThanOrEqualTo(DateTime.Today).WithMessage("StartTime must be today or in the future.");
+            //RuleFor(x => x.StartTime)
+            //    .NotNull().WithMessage("StartTime cannot be null.")
+            //    .GreaterThanOrEqualTo(DateTime.Today).WithMessage("StartTime must be today or in the future.");
 
-            RuleFor(x => x.EndTime)
-                .NotNull().WithMessage("EndTime cannot be null.")
-                .GreaterThanOrEqualTo(x => x.StartTime).WithMessage("EndTime must be after StartTime.")
-                .When(x => x.EndTime.HasValue && x.StartTime.HasValue);
+            //RuleFor(x => x.EndTime)
+            //    .NotNull().WithMessage("EndTime cannot be null.")
+            //    .GreaterThanOrEqualTo(x => x.StartTime).WithMessage("EndTime must be after StartTime.")
+            //    .When(x => x.EndTime.HasValue && x.StartTime.HasValue);
 
             RuleFor(x => x.TotalQuantityVoucher)
                 .NotNull().WithMessage("TotalQuantityVoucher cannot be null.")
